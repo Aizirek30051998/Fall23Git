e42887801607c3c513287891777e6d1c5a051001
Aizirek <aizirek.nadyrbek@gmail.com>
2024-01-25 10:35:53 +0600

    Revert "abs"
    This reverts commit 5b780a3ebf9d5be2724445f561bc97fc5a5ddf0d.

--- src/c/C1.java
+++ src/c/C1.java
@@ -3,7 +3,5 @@ package c;
 public class C1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-    }public static void sayHello(){
-        System.out.println("Hi");
     }
 }
